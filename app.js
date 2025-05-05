a7a9cedbd51025ad6fd2c402aa57cf75bd3965bd
subhadip dey <127054653+subhadip5069@users.noreply.github.com>
2025-05-05 16:15:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const session = require("express-session");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+const connectDB = require("./app/config/DB0000");
+
+
+require("dotenv").config();
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+
+app.use(session({
+  secret: 'educate-mysecretkey', // use a strong secret in production
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 } // 1 minute (adjust as needed)
+}));
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Set up EJS
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// Public folder
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static("uploads"));
+
+
+// Routesuser
+app.use("/", require("./app/routes/user/index"));
+
+// admin routes
+app.use("/", require("./app/routes/admin/index"));
+
+// teacher routes
+app.use("/", require("./app/routes/teachers/teachers.routes"));
+
+
+
+// Start server after database connections are established
+const PORT =  3265;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`http://localhost:${PORT} `);
+
+});
